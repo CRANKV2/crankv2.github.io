81238890f55d08ae0fa36792aad892e386d01b77
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2023-08-02 19:01:12 +0200

    Add files via upload

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-  function toggleMenu() {
-    const menuSidebar = document.getElementById('menuSidebar');
-    const content = document.querySelector('.content');
-
-    menuSidebar.classList.toggle('menu-open');
-    content.classList.toggle('menu-open');
-  }
