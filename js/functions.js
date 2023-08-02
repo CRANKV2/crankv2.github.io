a847f3894a59ffdff9e24e7c725fe17cfe301904
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2023-08-03 00:36:07 +0200

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+// hide the splash screen after a certain time
+    setTimeout(() => {
+      document.getElementById('splashScreen').style.display = 'none';
+    }, 3000); // Change the value (in milliseconds) to adjust the splash screen display time
